33b1a05369fcc3bbeaa1e9bb12d7b2306f03641a
fangkun <1416410300@qq.com>
2019-05-22 20:12:39 +0800

    project init commit

---
+++ src/main/java/com/supermall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.supermall.dao;
+
+import com.supermall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/supermall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.supermall.dao;
+
+import com.supermall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}

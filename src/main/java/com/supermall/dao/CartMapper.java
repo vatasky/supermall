866d5dce09374ac5e232139b37db86950570baf5
fangkun <1416410300@qq.com>
2019-07-11 12:52:50 +0800

    更新购物车

--- src/main/java/com/supermall/dao/CartMapper.java
+++ src/main/java/com/supermall/dao/CartMapper.java
@@ -1,6 +1,9 @@
 package com.supermall.dao;
 
 import com.supermall.pojo.Cart;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface CartMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +17,16 @@ public interface CartMapper {
     int updateByPrimaryKeySelective(Cart record);
 
     int updateByPrimaryKey(Cart record);
+
+    Cart selectByuserIdAndProductId(@Param("userId") Integer userId,@Param("productId") Integer productId);
+
+    List<Cart> selectCartByUserId(Integer userId);
+
+    int selectCartProductCheckedStatusByUserId(Integer userId);
+
+    int deleteProductByUserIdAndProductIds(@Param("userId") Integer userId,@Param("productIdList") List<String> productIdList);
+
+    int selectAndUnselect(@Param("userId") Integer userId,@Param("productId") Integer productId,@Param("checked") Integer checked);
+
+    int getCartProductCount(Integer userId);
 }

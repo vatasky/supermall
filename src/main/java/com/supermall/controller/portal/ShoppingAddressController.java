8630039b7bad0c0d9e9c28d2813bbbdbe3157ddb
fangkun <1416410300@qq.com>
2019-07-15 15:51:04 +0800

    收货地址的增删改查

---
+++ src/main/java/com/supermall/controller/portal/ShoppingAddressController.java
@@ -0,0 +1,113 @@
+package com.supermall.controller.portal;
+
+import com.github.pagehelper.PageInfo;
+import com.supermall.common.Const;
+import com.supermall.common.ResponseCode;
+import com.supermall.common.ServerResponse;
+import com.supermall.pojo.Shipping;
+import com.supermall.pojo.User;
+import com.supermall.service.IShoppingAddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author vatasky
+ */
+@Controller
+@RequestMapping("/shoppingAddress/")
+public class ShoppingAddressController {
+
+    @Autowired
+    private IShoppingAddressService iShoppingAddressService;
+
+    /**
+     * 添加收货地址
+     * @param session
+     * @param shipping
+     * @return
+     */
+    @RequestMapping("add.do")
+    @ResponseBody
+    public ServerResponse add(HttpSession session,Shipping shipping){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，需要管理员登录");
+        }
+
+        return iShoppingAddressService.add(user.getId(),shipping);
+    }
+
+    /**
+     * 删除地址
+     * @param session
+     * @param shippingId
+     * @return
+     */
+    @RequestMapping("delete.do")
+    @ResponseBody
+    public ServerResponse delete(HttpSession session,Integer shippingId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，需要管理员登录");
+        }
+
+        return iShoppingAddressService.delete(user.getId(),shippingId);
+    }
+
+    /**
+     * 更新地址
+     * @param session
+     * @param shipping
+     * @return
+     */
+    @RequestMapping("update.do")
+    @ResponseBody
+    public ServerResponse update(HttpSession session,Shipping shipping){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，需要管理员登录");
+        }
+
+        return iShoppingAddressService.update(user.getId(),shipping);
+    }
+
+    /**
+     * 查询地址详情
+     * @param session
+     * @param shippingId
+     * @return
+     */
+    @RequestMapping("select.do")
+    @ResponseBody
+    public ServerResponse select(HttpSession session,Integer shippingId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，需要管理员登录");
+        }
+
+        return iShoppingAddressService.select(user.getId(),shippingId);
+    }
+
+    /**
+     * 查询地址列表 并分页
+     * @param session
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    public ServerResponse<PageInfo> list(HttpSession session,
+                                         @RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
+                                         @RequestParam(value = "pageSize",defaultValue = "10")int pageSize){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，需要管理员登录");
+        }
+        return iShoppingAddressService.list(user.getId(),pageNum,pageSize);
+    }
+
+}
